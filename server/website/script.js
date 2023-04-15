7505df2913562ae35661b2dbfbc144a9c3dce4ea
karol <01158930@pw.edu.pl>
2023-04-15 20:12:01 +0200

    simple website

---
+++ server/website/script.js
@@ -0,0 +1,22 @@
+
+
+function fetchPackages() {
+    fetch('http://localhost:8000/packages')
+      .then(response => response.json())
+      .then(data => {
+        const packages = data.map(package => `
+          <tr>
+            <td>${package.id}</td>
+            <td>${package.height}</td>
+            <td>${package.destination}</td>
+          </tr>
+        `).join('');
+  
+        document.querySelector("#package-list").innerHTML = packages;
+      })
+      .catch(error => console.error(error));
+  }
+  
+  // Fetch packages every 2 seconds
+  setInterval(fetchPackages, 2000);
+  
