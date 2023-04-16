2741db5186c7fa2ed1386eb08d4f4e17d1dc4f57
lukileczo <lukileczo@gmail.com>
2023-04-16 11:24:29 +0200

    update tasmociag

--- tasmociag/main.py
+++ tasmociag/main.py
@@ -10,25 +10,25 @@ BUTTON_PIN = 12
 SERVO_PIN = 35
 
 GPIO.setmode(GPIO.BOARD)
-button_pressed = False
+# button_pressed = False
 
 GPIO.setup(SERVO_PIN, GPIO.OUT, initial=False)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 pwm = GPIO.PWM(SERVO_PIN, 50)
+pwm.start(7.7)
 
-def button_cb(channel):
-    global button_pressed, pwm
-    if button_pressed == False:
-        pwm.start(0)
-        pwm.ChangeDutyCycle(7.7)
+# def button_cb(channel):
+#     global button_pressed, pwm
+#     if button_pressed == False:
+#         pwm.start(7.7)
 
-        button_pressed = True
-        return
-    pwm.stop()
-    button_pressed = False
+#         button_pressed = True
+#         return
+#     pwm.stop()
+#     button_pressed = False
 
 
-GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_cb, bouncetime=50)
+# GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_cb, bouncetime=50)
 signal.signal(signal.SIGINT, signal_handler)
 
 while True:
